770742c2ac4778cae1cf313d484a4986157dcb0e
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-23 23:42:05 -0700

    Create Program.cs

---
+++ Practical 5/Practical5B/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(
+builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
